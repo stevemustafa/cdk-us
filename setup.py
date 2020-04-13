e4e9dcf1082bdecb50b0c5b048780f7fc4573652
Steven Mustafa <stemusta@amazon.ae>
2020-04-13 13:25:28 +0400

    initial commit + installation of PyPi libs

--- setup.py
+++ setup.py
@@ -4,22 +4,33 @@ import setuptools
 with open("README.md") as fp:
     long_description = fp.read()
 
-
 setuptools.setup(
-    name="url_shortener",
+    name="url shortener",
     version="0.0.1",
 
-    description="An empty CDK Python app",
+    description="A demo URL shortener application based on Lambda, DynamoDB and is built with CDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    author="author",
+    author="Steven Mustafa",
 
     package_dir={"": "url_shortener"},
     packages=setuptools.find_packages(where="url_shortener"),
 
     install_requires=[
         "aws-cdk.core==1.32.1",
+        "aws-cdk.aws-dynamodb",
+        "aws-cdk.aws-events",
+        "aws-cdk.aws-events-targets",
+        "aws-cdk.aws-lambda",
+        "aws-cdk.aws-s3",
+        "aws-cdk.aws-ec2",
+        "aws-cdk.aws-ecs-patterns",
+        "aws-cdk.aws-certificatemanager",
+        "aws-cdk.aws-apigateway",
+        "aws-cdk.aws-cloudwatch",
+        "Cdk-watchful",
+        "boto3"
     ],
 
     python_requires=">=3.6",
@@ -43,3 +54,4 @@ setuptools.setup(
         "Typing :: Typed",
     ],
 )
+
