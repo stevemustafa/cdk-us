0ea28ea7af2ea52075e106834c9a32c889980faa
Steven Mustafa <stemusta@amazon.ae>
2020-04-13 20:01:10 +0400

    Corrected the lambda function and added the API Gateway as well as completed the wireup

--- url_shortener/url_shortener_stack.py
+++ url_shortener/url_shortener_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core, aws_dynamodb
+from aws_cdk import core, aws_dynamodb, aws_lambda, aws_apigateway
 
 
 class UrlShortenerStack(core.Stack):
@@ -8,8 +8,40 @@ class UrlShortenerStack(core.Stack):
 
         # The code that defines your stack goes here
 
-        '''The below line, with only one line of code, will create a DynamoDB table that is name "url-shortener-table" 
+        '''
+        The below line, with only one line of code, will create a DynamoDB table that is name "url-shortener-table" 
         as well as having one defined attribute of type String call "id"
         '''
         table = aws_dynamodb.Table(self, "url-shortner-table",
                                    partition_key=aws_dynamodb.Attribute(name="id", type=aws_dynamodb.AttributeType.STRING))
+
+        '''
+        As we did previously with creating DynamoDB, we now create a Lambda function.
+        Please take note of the following:
+        * the "runtime" is defined from the static class aws_lambda.Runtime.<Runtime>
+        * the handler is referencing the function "main" in the handler.py file
+        * Code refers to the bundling of code assets from the entire directory ./lambda
+        '''
+
+        function = aws_lambda.Function(self, 'backend',
+                                       runtime=aws_lambda.Runtime.PYTHON_3_7,
+                                       handler='handler.main',
+                                       code=aws_lambda.Code.asset('./lambda'))
+
+        '''
+        The following creates the tie in between the lambda function and the dynamodb table by granting read and write 
+        permissions to the function on the table itself
+        this also involves injecting environment variables
+        '''
+        # create the tie in between the lambda function and the DynamoDB table
+        table.grant_read_write_data(function)
+
+        # add the environment variable for the function to consume
+        function.add_environment("TABLE_NAME", table.table_name)
+
+        # create the API Gateway
+        '''
+        The following is the entirety of the construct of creating the API Gateway and having it front the previously 
+        created lambda function
+        '''
+        api = aws_apigateway.LambdaRestApi(self, 'api', handler = function)
